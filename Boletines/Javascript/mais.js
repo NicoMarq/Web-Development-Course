651f1a212980fbecf3496a0e11cdc02c7f2b2a19
Nicolás <nico.marquez@hotmail.com>
2021-06-07 11:52:51 -0300

    Agregamos el Proyecto Boletines

---
+++ Boletines/Javascript/mais.js
@@ -0,0 +1,51 @@
+
+
+//DOM
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+    console.log(link);
+});
+
+
+
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener("click",function(){
+        console.log(this);
+    })
+});
+
+*/
+//PASOS
+//Obtener los elementos de la clase .close
+
+let links = document.querySelectorAll(".close");
+
+
+//Recorrerlos
+
+links.forEach(function(link){
+
+   //Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content')
+
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+        
+        content.classList.add("animate__fadeOutDown");
+        content.classList.add("animate__animated");
+       
+        setTimeout(function(){
+            location.href = "/";
+        },600);
+     
+
+        return false;
+      
+    });
+});
