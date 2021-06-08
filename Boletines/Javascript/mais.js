014e41cf8be385b192bbbee7c0af93518d0c1e56
Nicolás <nico.marquez@hotmail.com>
2021-06-08 15:49:33 -0300

    Boletines rev5

--- Boletines/Javascript/mais.js
+++ Boletines/Javascript/mais.js
@@ -41,7 +41,7 @@ links.forEach(function(link){
         content.classList.add("animate__animated");
        
         setTimeout(function(){
-            location.href = "/"
+            location.href = "/Boletines"
         },600);
      
 
